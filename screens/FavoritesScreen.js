15fdf87f5248296d8fb50357bd4defaeeeafb910
Molnar Dávid <molnardavid2002@gmail.com>
2023-06-05 20:19:36 +0200

    Favorites screen is added

---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function FavoritesScreen() {
+  return (
+    <View>
+      <Text>FAVORITES </Text>
+    </View>
+  );
+}
+
+export default FavoritesScreen;

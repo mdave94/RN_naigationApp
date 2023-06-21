2a63fc00ec20014cbfe1d582e9eecb8615505743
Molnar Dávid <molnardavid2002@gmail.com>
2023-06-21 23:20:55 +0200

    FavoriteScreen is updated the empty option

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,3 +1,4 @@
+import { View, Text, StyleSheet } from "react-native";
 import { useContext } from "react";
 import MealsList from "../components/MealsLIst/MealsList";
 import { FavoritesContext } from "../store/context/favorites-context";
@@ -10,7 +11,28 @@ function FavoritesScreen() {
     favoriteMealsContext.ids.includes(meal.id)
   );
 
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You not have favorite meals yet !</Text>
+      </View>
+    );
+  }
+
   return <MealsList items={favoriteMeals} />;
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
